856ee44f2a2b2151a22798007db660d6cde4dca3
BuildTools <unconfigured@null.spigotmc.org>
2023-10-13 19:31:57 +0200

    work done on the HTMLRenderer class.

--- HallwayInfoPanelGMH/HTMLRenderer.cs
+++ HallwayInfoPanelGMH/HTMLRenderer.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace HallwayInfoPanelGMH {
   class HTMLRenderer {
 
+    private const string htmlBeginning = "<!DOCTYPE html><html><head><title>GMH Infopanel</title><meta charset=\"UTF-16\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><style>";
+    private string CSS;
+    private const string htmlMiddle = "</style><body>";
+    private string body;
+    private const string htmlEnding = "</body></html>";
+
     public enum DisplayTypes {
       CLASSROOMS, INFO, WARNING
     }
@@ -17,6 +23,21 @@ namespace HallwayInfoPanelGMH {
 
       this.type = type;
 
+      //TODO intialize the renderer to use different elements based on the type
+
+    }
+
+    public void Render() {
+      switch (type) {
+        case DisplayTypes.CLASSROOMS:
+          break;
+        case DisplayTypes.INFO:
+          break;
+        case DisplayTypes.WARNING:
+          break;
+        default:
+          break; //TODO fill in the switch to render different values depending on the thing we wanna render
+      }
     }
   }
 }
