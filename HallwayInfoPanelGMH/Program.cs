3eedadcd274ff5540b34eafcdae22e20851f13ef
Jáchym Řezáč <jachym.rezac@gmail.com>
2023-10-15 16:12:03 +0200

    Preparing some debugging nonsense.

--- HallwayInfoPanelGMH/Program.cs
+++ HallwayInfoPanelGMH/Program.cs
@@ -2,7 +2,16 @@
 namespace HallwayInfoPanelGMH {
   internal class Program {
     static void Main(string[] args) {
-      //TODO: initialize all the classes and make a working product.
+      try{
+        string arg0 = args[0];
+      } catch {
+        Console.WriteLine("Usage: HallwayInfoPanelGMH.exe <Hallway ID>");
+        return;
+      }
+      if (args[0] == "--help" || args[0] == "-h") Console.WriteLine("Usage: HallwayInfoPanelGMH.exe <Hallway ID>"); return;
+
+
+      
     }
   }
 }
