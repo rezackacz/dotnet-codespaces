8957bf0448f207c9e8a39035b9c544a3e718575a
Jáchym Řezáč <jachym.rezac@student.gmh.cz>
2023-10-23 08:55:56 +0200

    Working on the deserialization of the XML food list.

--- HallwayInfoPanelGMH/CanteenMenuDownloader.cs
+++ HallwayInfoPanelGMH/CanteenMenuDownloader.cs
@@ -5,13 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
 
 namespace HallwayInfoPanelGMH {
   internal class CanteenMenuDownloader {
 
-    public CanteenMenuDownloader() {
-       
+    private string URL;
+    private XDocument xml;
+
+    public CanteenMenuDownloader(string URL) {
+      this.URL = URL;
+
+      this.xml = XDocument.Load(URL);
+
+      XNode jidenicek = (XElement)xml.FirstNode;
     }
 
+    public class Today {
+      public List<Jidlo> dnesniJidla = new List<Jidlo>();
+    }
+
+    public class Jidlo {
+      public string nazev { get; set; }
+      public string druh { get; set; }
+    }
+
+
+    public static void Main(string[] args) {
+      CanteenMenuDownloader downloader = new CanteenMenuDownloader("https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni=0059");
+      return;
+    }
   }
 }
+
+
