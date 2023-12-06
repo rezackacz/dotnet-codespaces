59785d2d5ba08bd45ccff39895de8ac4143501ca
Jáchym Řezáč <jachym.rezac@gmail.com>
2023-12-06 19:29:33 +0100

    bullshit this is

--- HallwayInfoPanelGMH/Classroom.cs
+++ HallwayInfoPanelGMH/Classroom.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace HallwayInfoPanelGMH {
   internal class Classroom {
 
-    public int id { get;}
-    public string dispName { get;}
-    public string? currentPeople {get;set;}
-    public string? subject {get;set;}
-    public string? currentTeacher {get;set;}
+    public int id { get; }
+    public string dispName { get; }
+    public string? currentPeople { get; set; }
+    public string? subject { get; set; }
+    public string? currentTeacher { get; set; }
 
     public Classroom(int id, string dispName) {
       this.id = id;
@@ -26,5 +26,19 @@ namespace HallwayInfoPanelGMH {
       this.subject = subject;
       this.currentTeacher = currentTeacher;
 
+    }
+
+
+    public string toString() {
+      string result;
+      result = "<div class=\"table-row\">";
+      result += "<div>" + this.dispName + "</div>";
+      result += "<div>" + this.currentPeople + "</div>";
+      result += "<div>" + this.subject + "</div>";
+      result += "<div>" + this.currentTeacher + "</div>";
+
+      return result;
+    }
+
   }
-}}
+}
